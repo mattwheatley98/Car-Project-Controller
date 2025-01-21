ca8057b108c4cc533c17c899bc869c6c15b69860
mattwheatley98 <113391095+mattwheatley98@users.noreply.github.com>
2025-01-21 10:23:55 -0500

    Update transmitTask.h

--- lib/src/transmitTask.h
+++ lib/src/transmitTask.h
@@ -2,10 +2,7 @@
 // Created by matt on 4/18/23.
 //
 
-#ifndef CARPROJECTCONTROLLER_TRANSMITTASK_H
-#define CARPROJECTCONTROLLER_TRANSMITTASK_H
-
-#endif //CARPROJECTCONTROLLER_TRANSMITTASK_H
+#pragma once
 
 void transmitTask(void *parameter);
 extern SemaphoreHandle_t toggleSemaphore;
